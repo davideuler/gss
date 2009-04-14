e2ce01a73af46a48e2405d7708be8d787e789306
pastith <pastith@localhost>
2009-04-14 10:31:17 +0000

    Fix automatic configuration reloading for files served through JBoss MC VFS.

--- gss/src/gr/ebs/gss/server/configuration/GSSConfigurationFactory.java
+++ gss/src/gr/ebs/gss/server/configuration/GSSConfigurationFactory.java
@@ -18,7 +18,12 @@
  */
 package gr.ebs.gss.server.configuration;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.ConfigurationUtils;
 import org.apache.commons.configuration.DataConfiguration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
@@ -78,10 +83,10 @@ public final class GSSConfigurationFactory {
 				gssConfig.setBasePath("");
 				gssConfig.setFileName(configFilename);
 				gssConfig.setEncoding("ISO-8859-7");
-				// Set automatic reloading
-				gssConfig.setReloadingStrategy(new FileChangedReloadingStrategy());
+				// Set automatic reloading.
+				gssConfig.setReloadingStrategy(new VfsFileChangedReloadingStrategy());
 				gssConfig.load();
-				// Decorator
+				// Decorator.
 				configuration = new DataConfiguration(gssConfig);
 			}
 			return configuration;
@@ -95,4 +100,61 @@ public final class GSSConfigurationFactory {
 		}
 	}
 
+	/**
+	 * Extends the FileChangedReloadingStrategy from Commons Configuration, adding
+	 * support for files in JBoss MC VFS.
+	 *
+	 * @author past
+	 */
+	private static class VfsFileChangedReloadingStrategy extends FileChangedReloadingStrategy {
+	    /** Constant for the jar URL protocol.*/
+	    private static final String JAR_PROTOCOL = "jar";
+	    /** Constant for the JBoss MC VFSFile URL protocol.*/
+	    private static final String VFSFILE_PROTOCOL = "vfsfile";
+
+	    @Override
+		protected File getFile()
+	    {
+	        return configuration.getURL() != null ? fileFromURL(configuration
+	                .getURL()) : configuration.getFile();
+	    }
+
+	    /**
+	     * Helper method for transforming a URL into a file object. This method
+	     * handles file: and jar: URLs, as well as JBoss VFS-specific vfsfile:
+	     * URLs.
+	     *
+	     * @param url the URL to be converted
+	     * @return the resulting file or <b>null </b>
+	     */
+	    private File fileFromURL(URL url)
+	    {
+	        if (VFSFILE_PROTOCOL.equals(url.getProtocol()))
+	        {
+	            String path = url.getPath();
+	            try
+	            {
+	                return ConfigurationUtils.fileFromURL(new URL("file:" + path));
+	            }
+	            catch (MalformedURLException mex)
+	            {
+	                return null;
+	            }
+	        }
+	        else if (JAR_PROTOCOL.equals(url.getProtocol()))
+	        {
+	            String path = url.getPath();
+	            try
+	            {
+	                return ConfigurationUtils.fileFromURL(new URL(path.substring(0,
+	                        path.indexOf('!'))));
+	            }
+	            catch (MalformedURLException mex)
+	            {
+	                return null;
+	            }
+	        } else
+				return ConfigurationUtils.fileFromURL(url);
+	    }
+	}
 }
