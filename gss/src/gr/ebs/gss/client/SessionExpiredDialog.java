d5781ecc33580adaa77a63d936ce239920bdfa69
pastith <pastith@localhost>
2009-04-24 09:50:23 +0000

    Add forgotten GPL notice and fix a warning.

--- gss/src/gr/ebs/gss/client/SessionExpiredDialog.java
+++ gss/src/gr/ebs/gss/client/SessionExpiredDialog.java
@@ -1,5 +1,20 @@
 /*
  * Copyright 2009 Electronic Business Systems Ltd.
+ *
+ * This file is part of GSS.
+ *
+ * GSS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GSS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSS.  If not, see <http://www.gnu.org/licenses/>.
  */
 package gr.ebs.gss.client;
 
@@ -47,11 +62,7 @@ public class SessionExpiredDialog extends DialogBox {
 		setWidget(outer);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see com.google.gwt.user.client.ui.PopupPanel#onKeyDownPreview(char, int)
-	 */
+	@Override
 	public boolean onKeyDownPreview(char key, int modifiers) {
 		// Use the popup's key preview hooks to close the dialog when either
 		// enter or escape is pressed.
@@ -64,7 +75,6 @@ public class SessionExpiredDialog extends DialogBox {
 				hide();
 				break;
 		}
-
 		return true;
 	}
 
