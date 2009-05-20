28e23ff15c2d651a5d93d787a9e4c2fcf5c4fe65
pastith <pastith@localhost>
2009-05-20 11:43:19 +0000

    Bring in fixes from tomcat 6.0.x up to r739532 (or tomcat trunk r734734): - Remove duplicate definitions and make normalize consistent

--- gss/src/gr/ebs/gss/server/webdav/Webdav.java
+++ gss/src/gr/ebs/gss/server/webdav/Webdav.java
@@ -1539,7 +1539,7 @@ public class Webdav extends HttpServlet {
 		if (!toAppend.startsWith("/"))
 			toAppend = "/" + toAppend;
 
-		generatedXML.writeText(rewriteUrl(normalize(absoluteUri + toAppend)));
+		generatedXML.writeText(rewriteUrl(RequestUtil.normalize(absoluteUri + toAppend)));
 
 		generatedXML.writeElement(null, "href", XMLWriter.CLOSING);
 
@@ -1689,63 +1689,6 @@ public class Webdav extends HttpServlet {
 		}
 
 		generatedXML.writeElement(null, "response", XMLWriter.CLOSING);
-
-	}
-
-	/**
-	 * Return a context-relative path, beginning with a "/", that represents the
-	 * canonical version of the specified path after ".." and "." elements are
-	 * resolved out. If the specified path attempts to go outside the boundaries
-	 * of the current context (i.e. too many ".." path elements are present),
-	 * return <code>null</code> instead.
-	 *
-	 * @param path Path to be normalized
-	 * @return the normalized path
-	 */
-	private String normalize(String path) {
-		if (path == null)
-			return null;
-
-		// Create a place for the normalized path
-		String normalized = path;
-		if (normalized.equals("/."))
-			return "/";
-
-		// Normalize the slashes and add leading slash if necessary
-		if (normalized.indexOf('\\') >= 0)
-			normalized = normalized.replace('\\', '/');
-		if (!normalized.startsWith("/"))
-			normalized = "/" + normalized;
-
-		// Resolve occurrences of "//" in the normalized path
-		while (true) {
-			int index = normalized.indexOf("//");
-			if (index < 0)
-				break;
-			normalized = normalized.substring(0, index) + normalized.substring(index + 1);
-		}
-
-		// Resolve occurrences of "/./" in the normalized path
-		while (true) {
-			int index = normalized.indexOf("/./");
-			if (index < 0)
-				break;
-			normalized = normalized.substring(0, index) + normalized.substring(index + 2);
-		}
-
-		// Resolve occurrences of "/../" in the normalized path
-		while (true) {
-			int index = normalized.indexOf("/../");
-			if (index < 0)
-				break;
-			if (index == 0)
-				return null; // Trying to go outside our context
-			int index2 = normalized.lastIndexOf('/', index - 1);
-			normalized = normalized.substring(0, index2) + normalized.substring(index + 3);
-		}
-
-		// Return the normalized path that we have completed
-		return normalized;
 
 	}
 
@@ -3419,8 +3362,8 @@ public class Webdav extends HttpServlet {
 			}
 		}
 
-		// Normalise destination path (remove '.' and '..')
-		destinationPath = normalize(destinationPath);
+		// Normalize destination path (remove '.' and '..')
+		destinationPath = RequestUtil.normalize(destinationPath);
 
 		String contextPath = req.getContextPath();
 		if (contextPath != null && destinationPath.startsWith(contextPath))
