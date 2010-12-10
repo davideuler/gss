d652bcbae9587ac3f5b7b94cae5d7668ae7bad2a
Natasa Kapravelou <akapravelou@gmail.com>
2010-12-10 12:41:49 +0200

    Decorative changes related to Issue 64

--- src/gr/ebs/gss/server/domain/UserLogin.java
+++ src/gr/ebs/gss/server/domain/UserLogin.java
@@ -20,9 +20,7 @@ package gr.ebs.gss.server.domain;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
