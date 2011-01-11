3e042fb68a4de96a975cfaf335aab48dc15c4dbf
koutsoub <koutsoub@localhost>
2011-01-11 14:29:53 +0200

    fix dnd bug, and use filelist.getSelectedFiles() on file drop

--- src/gr/ebs/gss/client/FileTable.java
+++ src/gr/ebs/gss/client/FileTable.java
@@ -22,6 +22,8 @@ import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Widget;
 
 
 /**
@@ -56,6 +58,30 @@ public class FileTable extends Grid{
 	    Element body = DOM.getParent(tr);
 	    int row = DOM.getChildIndex(body, tr);
 	    return row;
+	  }
+	
+	public static void copyRow(FileTable sourceTable, FileTable targetTable, int sourceRow, int targetRow) {
+		targetTable.insertRow(targetRow);
+		for (int col = 0; col < sourceTable.getCellCount(sourceRow); col++) {
+			HTML html = new HTML(sourceTable.getHTML(sourceRow, col));
+			targetTable.setWidget(targetRow, col, html);
+		}
+		copyRowStyle(sourceTable, targetTable, sourceRow, targetRow);
+	}
+
+	private static void copyRowStyle(FileTable sourceTable, FileTable targetTable, int sourceRow, int targetRow) {
+		String rowStyle = sourceTable.getRowFormatter().getStyleName(sourceRow);
+		targetTable.getRowFormatter().setStyleName(targetRow, rowStyle);
+	}
+
+	public static int getWidgetRow(Widget widget, FileTable table) {
+	    for (int row = 0; row < table.getRowCount(); row++)
+			for (int col = 0; col < table.getCellCount(row); col++) {
+			    Widget w = table.getWidget(row, col);
+			    if (w == widget)
+					return row;
+			  }
+	    throw new RuntimeException("Unable to determine widget row");
 	  }
 
 }
