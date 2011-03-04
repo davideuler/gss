01351bd7a4f14ec3fcbda2d7cc9af0cacab045ce
Christos V. Stathis <chstath@ebs.gr>
2011-03-04 15:39:58 +0200

    Fixed version number

---
+++ src/gr/ebs/gss/server/webdav/milton/GssSecurityManager.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright 2011 Electronic Business Systems Ltd.
+ *
+ * This file is part of GSS.
+ *
+ * GSS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GSS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package gr.ebs.gss.server.webdav.milton;
+
+import static gr.ebs.gss.server.configuration.GSSConfigurationFactory.getConfiguration;
+import gr.ebs.gss.client.exceptions.RpcException;
+import gr.ebs.gss.server.domain.User;
+import gr.ebs.gss.server.ejb.ExternalAPI;
+import gr.ebs.gss.server.ejb.TransactionHelper;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.rmi.PortableRemoteObject;
+import javax.security.auth.login.FailedLoginException;
+import javax.security.auth.login.LoginException;
+
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bradmcevoy.http.Auth;
+import com.bradmcevoy.http.Request;
+import com.bradmcevoy.http.Resource;
+import com.bradmcevoy.http.Request.Method;
+import com.bradmcevoy.http.http11.auth.DigestGenerator;
+import com.bradmcevoy.http.http11.auth.DigestResponse;
+import com.ettrema.http.fs.SimpleSecurityManager;
+
+
+/**
+ * @author kman
+ *
+ */
+public class GssSecurityManager  implements com.bradmcevoy.http.SecurityManager{
+
+	private static final Logger log = LoggerFactory.getLogger(SimpleSecurityManager.class);
+
+    private String realm;
+    private DigestGenerator digestGenerator;
+
+    public GssSecurityManager() {
+        digestGenerator = new DigestGenerator();
+    }
+
+    public GssSecurityManager( DigestGenerator digestGenerator ) {
+        this.digestGenerator = digestGenerator;
+    }
+
+   
+    public GssSecurityManager( String realm) {
+        this.realm = realm;
+        this.digestGenerator = new DigestGenerator();
+    }
+    /*
+    public Object getUserByName( String name ) {
+        String actualPassword = nameAndPasswords.get( name );
+        if( actualPassword != null ) return name;
+        return null;
+    }*/
+
+
+
+    public Object authenticate( String user, String password ) {
+        //log.info( "authenticate: " + user + " - " + password);
+        // user name will include domain when coming form ftp. we just strip it off
+        if( user.contains( "@")) {
+            user = user.substring( 0, user.indexOf( "@"));
+        }
+        String actualPassword=null;
+		try {
+			actualPassword = getUsersPassword( user );
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+        if( actualPassword == null ) {
+            log.debug( "user not found: " + user);
+            return null;
+        } else {
+            boolean ok;
+            if( actualPassword == null ) {
+                ok = password == null || password.length()==0;
+            } else {
+                ok = actualPassword.equals( password);
+            }
+            return ok ? user : null;
+        }
+    }
+
+    public Object authenticate( DigestResponse digestRequest ) {
+    	//log.info( "DIGEST authenticate: " + digestRequest);
+        String actualPassword=null;
+		try {
+			actualPassword = getUsersPassword( digestRequest.getUser() );
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		
+        String serverResponse = digestGenerator.generateDigest( digestRequest, actualPassword );
+        String clientResponse = digestRequest.getResponseDigest();
+
+        if( serverResponse.equals( clientResponse ) ) {
+            try {
+				return getService().getUserByUserName(digestRequest.getUser());
+			} catch (RpcException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+        } else {
+            return null;
+        }
+    }
+
+
+
+    public boolean authorise( Request request, Method method, Auth auth, Resource resource ) {
+        return auth != null && auth.getTag() != null;
+    }
+
+    public String getRealm(String host) {
+        return realm;
+    }
+
+    /**
+     * @param realm the realm to set
+     */
+    public void setRealm( String realm ) {
+        this.realm = realm;
+    }
+    
+    private ExternalAPI getService() throws RpcException {
+		try {
+			final Context ctx = new InitialContext();
+			final Object ref = ctx.lookup(getConfiguration().getString("externalApiPath"));
+			return (ExternalAPI) PortableRemoteObject.narrow(ref, ExternalAPI.class);
+		} catch (final NamingException e) {
+			log.error("Unable to retrieve the ExternalAPI EJB", e);
+			throw new RpcException("An error occurred while contacting the naming service");
+		}
+	}
+
+	
+	protected String getUsersPassword(String username) throws Exception {
+		
+		try {
+			final User user = getService().findUser(username);
+			if (user == null) throw new FailedLoginException("User '" + username + "' not found.");
+			if (!user.isActive()) throw new FailedLoginException("User '" + username + "' is disabled.");
+			if (user.getWebDAVPassword() != null && user.getWebDAVPassword().length() > 0)
+				return user.getWebDAVPassword();
+			// If no password has ever been generated, use token instead
+			String tokenEncoded = new String(Base64.encodeBase64(user.getAuthToken()), "US-ASCII");
+			user.setWebDAVPassword(tokenEncoded);
+			new TransactionHelper<Void>().tryExecute(new Callable<Void>() {
+				@Override
+				public Void call() throws Exception {
+					getService().updateUser(user);
+					return null;
+				}
+			});
+			return tokenEncoded;
+		} catch (RpcException e) {
+			String error = "An error occurred while communicating with the service";
+			log.error(error, e);
+			throw new Exception(e.getMessage());
+		} catch (UnsupportedEncodingException e) {
+            log.error("", e);
+            throw new Exception(e.getMessage());
+		} catch (Exception e) {
+            log.error("", e);
+			throw new Exception(e.getMessage());
+		}
+	}
+
+    
+
+
+}
---
+++ war/register.jsp
@@ -0,0 +1,93 @@
+<%--
+
+Copyright 2010 Electronic Business Systems Ltd.
+
+This file is part of GSS.
+
+GSS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GSS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GSS.  If not, see <http://www.gnu.org/licenses/>.
+
+--%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<%@page import="gr.ebs.gss.server.configuration.GSSConfigurationFactory"%>
+<%@page import="java.net.URLDecoder"%>
+<html>
+<head>
+<title><%= GSSConfigurationFactory.getConfiguration().getString("serviceName") %> Registration</title>
+<link href="/pithos/main.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<div class="wrapper" >
+<div class="header"></div>
+<div class="image_logo">
+<table><tr>
+  <td><a href="/"><img src="/pithos/images/service-logo.png"></img></a>
+</tr></table>
+</div> <!-- image_logo -->
+<div style="clear: both; "> </div>
+<div class="page_main">
+<center>
+<p class="blurb">Welcome to the <%= GSSConfigurationFactory.getConfiguration().getString("serviceName") %> storage service.
+Create an account with the service by filling and submitting the following form. Please note that all fields are required.</p>
+<form method="post" action="/pithos/register">
+<div class="error">
+    <%= request.getParameter("error") != null? URLDecoder.decode(request.getParameter("error"), "UTF-8"): "" %>
+</div>
+    <table>
+        <tr>
+            <td>Firstname:</td>
+            <td><input type="text" name="firstname" title="Enter a firstname" value="<%= request.getParameter("firstname") != null? URLDecoder.decode(request.getParameter("firstname"), "UTF-8"): ""  %>"/></td>
+        </tr>
+        <tr>
+            <td>Lastname:</td>
+            <td><input type="text" name="lastname" title="Enter a lastname" value="<%= request.getParameter("lastname") != null? URLDecoder.decode(request.getParameter("lastname"), "UTF-8"): ""  %>"/></td>
+        </tr>
+        <tr>
+            <td>E-Mail:</td>
+            <td><input type="text" name="email" title="Enter an e-mail address" value="<%= request.getParameter("email") != null? URLDecoder.decode(request.getParameter("email"), "UTF-8"): "" %>"/></td>
+        </tr>
+        <tr>
+            <td>Username:</td>
+            <td><input type="text" name="username" title="Enter a username" value="<%= request.getParameter("username") != null? URLDecoder.decode(request.getParameter("username"), "UTF-8"): "" %>"/></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="password" title="Enter a password"/></td>
+        </tr>
+        <tr>
+            <td>Confirm Password:</td>
+            <td><input type="password" name="password2" title="Enter the password again to confirm"/></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <input type="checkbox" name="accept" title="Accept terms and conditions"/>
+                I have read and understood the <a href='/terms' target='_blank'>
+                Terms and Conditions</a> of the Service and agree to abide by them
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <input type="submit" value="Sign Up" />
+            </td>
+        </tr>
+    </table>
+</form>
+</center>
+</div>
+<div class="footer"></div>
+</div> <!-- wrapper -->
+</body>
+</html>
