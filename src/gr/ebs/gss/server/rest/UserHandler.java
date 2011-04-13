66f85ffecc9824e248065e41a570b50f7a03f460
Christos V. Stathis <chstath@ebs.gr>
2011-04-13 13:24:28 +0300

    Fixed display of the AUTH users message along with other announcements

--- src/gr/ebs/gss/server/rest/UserHandler.java
+++ src/gr/ebs/gss/server/rest/UserHandler.java
@@ -92,9 +92,11 @@ public class UserHandler extends RequestHandler {
 					put("shared", parentUrl + PATH_SHARED).put("others", parentUrl + PATH_OTHERS).
 					put("quota", statistics).put("tags", parentUrl + PATH_TAGS);
 			String announcement = getConfiguration().getString("announcement", "");
+            if (announcement.length() > 0)
+                announcement = "<p>" + announcement + "</p>";
             String authgr = getConfiguration().getString("authgr", "auth.gr");
             if (authgr.equals(user.getHomeOrganization()))
-                announcement += getConfiguration().getString("authAnnouncement", "");
+                announcement += "<p>" + getConfiguration().getString("authAnnouncement", "") + "</p>";
 			if (announcement != null && !announcement.isEmpty())
 				json.put("announcement", announcement);
 			List<UserLogin> userLogins = getService().getLastUserLogins(owner.getId());			
