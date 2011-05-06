b99689e1fe2a7a7e6a8f8b11e637924f08ac5c99
Christos V. Stathis <chstath@ebs.gr>
2011-05-06 13:23:08 +0300

    Hardcoded pithos web site url

--- src/org/gss_project/gss/web/client/TopPanel.java
+++ src/org/gss_project/gss/web/client/TopPanel.java
@@ -195,12 +195,7 @@ public class TopPanel extends Composite {
 
 		outer.add(loading);
 
-		Configuration conf = (Configuration) GWT.create(Configuration.class);
-        String path = Window.Location.getPath();
-        String baseUrl = GWT.getModuleBaseURL();
-        String homeUrl = baseUrl.substring(0, baseUrl.indexOf(path));
-		HTML logos = new HTML("<table><tr><td><a href='" + homeUrl +
-					"' target='gss'>" +	AbstractImagePrototype.create(images.gssLogo()).getHTML() +
+		HTML logos = new HTML("<table><tr><td><a href='http://pithos.grnet.gr' target='gss'>" +	AbstractImagePrototype.create(images.gssLogo()).getHTML() +
 					"</a><a href='http://www.grnet.gr/' " +	"target='grnet'>" +
 					AbstractImagePrototype.create(images.grnetLogo()).getHTML()+"</a></td></tr></table>");
 		outer.add(logos);
